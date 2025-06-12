3a0b71313cc4a97865dcd5ecb8be39464942082d
ss2552 <85072753+ss2552@users.noreply.github.com>
2025-06-12 17:31:27 +0000

    asd

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,20 +1,18 @@
 #include <3ds.h>
 
 #include <stdio.h>
+#include <stdint.h>
 #include <string>
-#include <types.h>
-
-#define FB_BOTTOM_VRAM_ADDR         ((void *)0x1F48F000)
 
 namespace CTRPluginFramework{
     void main(void){
         
-        uint16_t *const fb = (uint16_t *)FB_BOTTOM_VRAM_ADDR;
+        uint16_t *const fb = (uint16_t *)((void *)0x1F48F000);
 
         int32_t y;
         for(y = 0; y < 30; y++)
         {
-            fb[(50 * 400 * 2 + (400 - y - 50 - 1) * 2) + (5 - x) * 2 * 400; / 2] = 0xFFFFFF;
+            fb[((50 * 400 * 2 + (400 - y - 50 - 1) * 2) + (5 - 3) * 2 * 400) / 2] = 0xFFFFFF;
         }
     }
 }
